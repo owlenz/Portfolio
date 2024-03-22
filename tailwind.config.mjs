b02bce2e2e107d22090cff5103112095e9072cf7
Saif Al-Din Samy Hussein <SeifOwleN@proton.me>
2024-03-23 01:49:55 +0200

    not much added fonts and tmrw i integrate some new design idea isa

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      animation: {
-        bgGlow: "bgGlow 2s linear infinte",
-      },
-      keyframes: {
-        bgGlow: {
-          to: { "background-position": "200% center" },
-        },
-      },
-    },
-    fontFamily: {
-      merriweather: ["Merriweather", "serif"],
-      ibm: ["IBM Plex Sans", "sans-serif"],
-      ubuntu: ["Ubuntu", "sans-serif"],
-      montagu: ["Montagu Slab", "serif"],
-      poppins: ["Poppins", "sans-serif"],
-      inter: ["Inter", "sans-serif"],
-      expose: ["Expose", "sans-serif"],
-      persona: ["Persona", "sans-serif"],
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			animation: {
+				bgGlow: "bgGlow 2s linear infinte",
+			},
+			keyframes: {
+				bgGlow: {
+					to: { "background-position": "200% center" },
+				},
+			},
+		},
+		fontFamily: {
+			merriweather: ["Merriweather", "serif"],
+			ibm: ["IBM Plex Sans", "sans-serif"],
+			ubuntu: ["Ubuntu", "sans-serif"],
+			montagu: ["Montagu Slab", "serif"],
+			poppins: ["Poppins", "sans-serif"],
+			inter: ["Inter", "sans-serif"],
+			expose: ["Expose", "sans-serif"],
+			persona: ["Persona", "sans-serif"],
+			optima: ["Optima", "sans-serif"],
+			krsm: ["KoreanKRSM", "sans-serif"],
+		},
+	},
+	plugins: [],
 };
