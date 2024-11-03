c5cb18e9c1a954f6079d53687bd7b7fa4f691935
Saif Al-Din Samy Hussein <SeifOwleN@proton.me>
2024-11-03 23:28:39 +0200

    test

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Saif Al-Din";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Saif Al-Din's website";
+export const SITE_DESCRIPTION = "Saif Al-Din's (owlen) personal website";
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,9 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
 	type: "content",
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
@@ -21,6 +19,7 @@ const project = defineCollection({
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		images: z.string().array(),
+		// tags: z.string().array()
 	}),
 });
 
