81c89326b85f9adfbc5428403b9e0cce6c707f0b
Saif Al-Din Samy <seifowlen@proton.me>
2025-09-22 04:56:47 +0300

    updated github actions and url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://saifowlen.github.io/",
-	base: "/Portfolio",
+	site: "https://owlenz.xyz",
+	# base: "/Portfolio",
 	integrations: [
 		mdx(),
 		sitemap(),
